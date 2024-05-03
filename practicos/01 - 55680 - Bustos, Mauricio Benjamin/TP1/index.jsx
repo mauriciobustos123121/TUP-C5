4e14584e3d684602a27613e3d3462de78e010b87
mauriciobustos123121 <168594707+mauriciobustos123121@users.noreply.github.com>
2024-05-03 12:39:31 -0300

    Update index.jsx

--- practicos/01 - 55680 - Bustos, Mauricio Benjamin/TP1/index.jsx
+++ practicos/01 - 55680 - Bustos, Mauricio Benjamin/TP1/index.jsx
@@ -1,8 +1,42 @@
 const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
-    </div>
+  const = [
+    {
+     id:0,
+     Nombre:Mauricio,
+     Apellido:Bustos,
+     Telefono:3814659905
+    },
+    {
+     id:1,
+     Nombre:Luz,
+     Apellido:Moreno,
+     Telefono:3815845617
+    },
+    {
+     id:2,
+     Nombre:Gaspar,
+     Apellido:Salazar,
+     Telefono:3814659906   
+    },
+    {
+     id:3,
+     Nombre:Maria,
+     Apellido:Bertrand,
+     Telefono:3814652312
+    },
+    {
+     id:4,
+     Nombre:Fernando,
+     Apellido:Villafañe,
+     Telefono:3813555906
+    },
+    {
+     id:5,
+     Nombre:Tomas,
+     Apellido:Sanvitale,
+     Telefono:3814653218
+    }
+  ]
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
